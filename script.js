94337a9456a4ca7c486d02dda239599e8768fcb8
Muhammad Azam <muhammad.azam@introtechsolutions.com>
2022-01-31 23:38:15 +0500

    final commit

--- script.js
+++ script.js
@@ -20,27 +20,22 @@ function highlightBox(element, box) {
         case 1:
             totalScore = totalScore + 1;
             pointsArray.push(1)
-            window.alert(1)
             break;
         case 2:
             totalScore = totalScore + 2;
             pointsArray.push(2)
-            window.alert(2)
             break;
         case 3:
             totalScore = totalScore + 3;
             pointsArray.push(3)
-            window.alert(3)
             break;
         case 4:
             totalScore = totalScore + 4;
             pointsArray.push(4)
-            window.alert(4)
             break;
         case 5:
             totalScore = totalScore + 5;
             pointsArray.push(5)
-            window.alert(5)
             break;
         case 6:
             totalScore = totalScore + 6;
