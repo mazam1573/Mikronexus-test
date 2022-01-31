493c3d6b816f89e7c0867d623f569ffda05725f3
Muhammad Azam <muhammad.azam@introtechsolutions.com>
2022-01-31 23:30:04 +0500

    final Code

---
+++ script.js
@@ -0,0 +1,62 @@
+setInterval(() => {
+    location.reload()
+}, 120000);
+var totalScore = 0;
+let pointsArray = [];
+var audio = new Audio('beep.mp3');
+function highlightBox(element, box) {
+    var blocks = document.getElementsByClassName('circle');
+    const total = document.getElementById("totalScore");
+    const logs = document.getElementById('log');
+    for (var i = 0; i < blocks.length - 1; i++) {
+        blocks[i].style.background = "white";
+    }
+    if (box != 6 && box != 7 && box != 8) {
+        element.style.background = "green";
+    }
+    audio.play();
+
+    switch (box) {
+        case 1:
+            totalScore = totalScore + 1;
+            pointsArray.push(1)
+            window.alert(1)
+            break;
+        case 2:
+            totalScore = totalScore + 2;
+            pointsArray.push(2)
+            window.alert(2)
+            break;
+        case 3:
+            totalScore = totalScore + 3;
+            pointsArray.push(3)
+            window.alert(3)
+            break;
+        case 4:
+            totalScore = totalScore + 4;
+            pointsArray.push(4)
+            window.alert(4)
+            break;
+        case 5:
+            totalScore = totalScore + 5;
+            pointsArray.push(5)
+            window.alert(5)
+            break;
+        case 6:
+            totalScore = totalScore + 6;
+            pointsArray.push(6)
+            window.alert(6)
+            break;
+        case 7:
+        case 8:
+            totalScore = totalScore + 8;
+            pointsArray.push(8)
+            window.alert(8)
+            break;
+
+        default:
+            break;
+    }
+    total.innerHTML = "Score: " + totalScore;
+    logs.innerHTML = "BackLogs: " + pointsArray;
+}
